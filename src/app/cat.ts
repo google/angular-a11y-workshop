2a710b821c0ff73b593d52bc63577f5b17ec8466
Susie Sahim <ssahim@google.com>
2018-08-15 12:03:05 -0700

    feat(catalog): enhance display of catalog data
    - add images
    - design cards
    - display all information
    
    Photos obtained from Jason Barbour and Ashley Barbour and used with permission.

--- src/app/cat.ts
+++ src/app/cat.ts
@@ -2,20 +2,26 @@
 export interface Cat {
   name: string;
   description: string;
-  imageCredit: string;
+  breed: string;
+  imgUrl?: string;
+  imgCredit: string;
 }
 
 export type CatCatalog = {[key: number]: Cat};
 
 export const InitialCatData: CatCatalog = {
   1: {
-    name: 'Sphynx',
+    name: 'Odo',
+    breed: 'Sphynx',
     description: 'The Sphynx cat is a breed of cat known for its lack of coat. It was developed through selective breeding, starting in the 1960s. The skin should have the texture of chamois, as it has fine hairs.',
-    imageCredit: ''
+    imgCredit: 'Jason Barbour and Ashley Barbour. Instagram: @mostly_kitten_pics',
+    imgUrl: '/assets/photos/odo.png'
   },
   2: {
-    name: 'Maine Coon',
+    name: 'Kahless',
+    breed: 'Maine Coon',
     description: 'The Maine Coon is one of the largest domesticated breeds of cat. It has a distinctive physical appearance and valuable hunting skills.',
-    imageCredit: '',
+    imgCredit: 'Jason Barbour and Ashley Barbour. Instagram: @mostly_kitten_pics',
+    imgUrl: '/assets/photos/kahless.png'
   }
 };
